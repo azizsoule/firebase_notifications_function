70456cbb01f6af56b4ad161f7b81fcfd7d91aa64
Aziz Soulé <aziz0071999@gmail.com>
2020-04-27 15:04:17 +0100

    First commit

---
+++ functions/index.js
@@ -0,0 +1,87 @@
+//SECTION1:ADMIN SECTION THIS IS COMMAN FOR ALL THE FIREBASE  CODES
+
+
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+
+// SECION2:TRIGGERING SECTION THIS WILL WATCHING YOUR DATABASE DAY AND NIGHT
+
+
+exports.appTrigger = functions.firestore.document("discussion/{anydocument}").onCreate(async(change, context) => {
+
+    const receiver = change.data['destinataireUid'];
+    const sender = change.data['expediteurUid'];
+    const messageis = change.data['content'];
+    const type = change.data['type'];
+
+
+    //SECTION3:GETING THE FCM CODE OF RECEIVER 
+
+    const toUser = admin.firestore().collection("comptes").where('pseudo', '==', receiver).get();
+
+
+    return Promise.all([toUser]).then(async result => { // as it is in array you can perfom more promisses now just leavae it as it is
+
+        const tokenId = result[0].data['token']; //we are geting the his fcm tokenId
+
+
+        /////// SECTION4: PREPARING A NOTIFICATION
+
+        var notificationContent;
+
+        switch (type) {
+            case "text":
+                notificationContent = {
+                    notification: {
+                        icon: "default",
+                        title: sender, //we use the sender name to show in notification
+                        body: messageis, //we use the receiver's name and message to show in notifcation
+                        icon: "default", //you can change the icon on the app side too
+                        sound: "default" //also you can change the sound in app side
+                    }
+                };
+                break;
+
+            case "image":
+                notificationContent = {
+                    notification: {
+                        icon: "default",
+                        title: sender, //we use the sender name to show in notification
+                        body: "Photo", //we use the receiver's name and message to show in notifcation
+                        icon: "default", //you can change the icon on the app side too
+                        sound: "default" //also you can change the sound in app side
+                    }
+                };
+                break;
+
+            default:
+                notificationContent = {
+                    notification: {
+                        icon: "default",
+                        title: sender, //we use the sender name to show in notification
+                        body: "unknown", //we use the receiver's name and message to show in notifcation
+                        icon: "default", //you can change the icon on the app side too
+                        sound: "default" //also you can change the sound in app side
+                    }
+                };
+                break;
+        }
+
+
+
+        ///SECTION 5 SENDING  NOTIFICATIONS 
+
+
+        return admin.messaging().sendToDevice(
+            tokenId, //here we use the receiver's fcm token id to send notifcations
+            notificationContent //this is the notication content which is on the line 46 to 52
+        ).
+        then(result => {
+            console.log("Notification sent!");
+            //admin.firestore().collection("notifications").doc(userEmail).collection("userNotifications").doc(notificationId).delete();
+        });
+    });
+
+});
